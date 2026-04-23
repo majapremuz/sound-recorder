9c5b4d931b3acb41046ac87645623667b8b2f789
majapremuz <majchi@windowslive.com>
2026-04-23 20:50:04 +0200

    removed password length

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -63,6 +63,8 @@ export class LocationService {
   }
 
 resetSelectedCities() {
+  console.log('RESETTING CITIES');
   this.selectedCitiesSubject.next([]);
+  localStorage.removeItem('selectedCities');
 }
 }
